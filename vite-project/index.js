55ade995f03f700f5b8692a252569f72dcaa99d9
makshoop <makshoope@gmail.com>
2023-01-14 05:34:05 +0400

    add styles

---
+++ vite-project/index.js
@@ -0,0 +1,3 @@
+import "/Users/zaqza/.vscode/notes/vite-project/src/styles/main.css";
+import "/Users/zaqza/.vscode/notes/vite-project/src/styles/notes.css";
+import "/Users/zaqza/.vscode/notes/vite-project/src/styles/searcher.css";
