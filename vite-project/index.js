914c2072c5c3a8fb69c483f25f4bc9683333e886
makshoop <makshoope@gmail.com>
2023-01-19 21:57:08 +0400

    add load

--- vite-project/index.js
+++ vite-project/index.js
@@ -1,5 +1,5 @@
-import "/Users/zaqza/.vscode/notes/vite-project/src/styles/main.css";
-import "/Users/zaqza/.vscode/notes/vite-project/src/styles/notes.css";
-import "/Users/zaqza/.vscode/notes/vite-project/src/styles/searcher.css";
-import "/Users/zaqza/.vscode/notes/vite-project/src/js/notes";
-import "/Users/zaqza/.vscode/notes/vite-project/src/js/search";
+import "./src/styles/main.css";
+import "./src/styles/notes.css";
+import "./src/styles/searcher.css";
+import "./src/js/notes";
+import "./src/js/search";
--- vite-project/src/js/utils.js
+++ vite-project/src/js/utils.js
@@ -6,14 +6,22 @@ export function highlightWords(line, word) {
 // document.querySelector("#note").innerHTML = `
 // 	<h2>No results ;(</h2>`;
 
+let load = document.getElementById("input");
+
 export function throttle(callee, timeout) {
 	let timer = null;
 	let isTouchedBeforeTimerClear = false;
+
 	return function perform(...args) {
 		if (timer) {
 			isTouchedBeforeTimerClear = true;
+
 			return;
 		}
+		input.setAttribute(
+			"style",
+			"background: url(./src/assets/load.gif)  no-repeat center right; background-size: 20px;",
+		);
 
 		callee(...args);
 		clearTimeout(timer);
@@ -25,6 +33,7 @@ export function throttle(callee, timeout) {
 			}
 			isTouchedBeforeTimerClear = false;
 			timer = null;
+			input.setAttribute("style", "background: none;");
 		}, timeout);
 	};
 }
