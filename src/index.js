365a71ed782e8d48df65a4ff19a1c0f39f929e1c
makshoop <makshoope@gmail.com>
2023-01-20 17:59:14 +0400

    fix paths

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import "./src/styles/main.css";
-import "./src/styles/notes.css";
-import "./src/styles/searcher.css";
-import "./src/js/notes";
-import "./src/js/search";
+import "./styles/main.css";
+import "./styles/notes.css";
+import "./styles/searcher.css";
+import "./js/notes";
+import "./js/search";
