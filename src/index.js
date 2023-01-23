a392102c74f53d34b798d002737f8d90b89d00ac
makshoop <makshoope@gmail.com>
2023-01-23 22:32:18 +0400

    delete and edit notes functions

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import "./styles/main.css";
 import "./styles/notes.css";
 import "./styles/searcher.css";
+import "./styles/windows.css";
 import "./js/notes";
 import "./js/search";
-import "./styles/new-note.css";
--- src/js/notes.js
+++ src/js/notes.js
@@ -24,15 +24,15 @@ export function getNotes() {
 
 					return `
 					
-						<div class="note">
-							<div class="buttons">
-								<button id='delete-button'>
-									<img src="./src/assets/close.svg" alt="delete">
-								</button>
-								<button id='edit-button'>
-									<img src="/src/assets/pencil.svg" alt="edit">
-								</button>
-							</div>
+						<div class="note" data-note-id="${note.id}">
+						<div class="buttons">
+							<button class='delete-button' id="delete-button${note.id}">
+								<img src="./src/assets/delete.svg" alt="delete">
+							</button>
+							<button class="edit-button" id='edit-button'>
+								<img src="/src/assets/pencil.svg" alt="edit">
+							</button>
+						</div>
 							<h2>${title}</h2>
 							<p>${description}</p>
 						</div>
@@ -41,6 +41,16 @@ export function getNotes() {
 				document.querySelector("#notes").innerHTML = notes.join("");
 			}
 			input.setAttribute("style", "background: none;");
+
+			const deleteButtons = document.querySelectorAll(".delete-button");
+			deleteButtons.forEach((button) => {
+				button.addEventListener("click", deleteNote);
+			});
+
+			const editButtons = document.querySelectorAll(".edit-button");
+			editButtons.forEach((button) => {
+				button.addEventListener("click", editNote);
+			});
 		});
 }
 
@@ -55,12 +65,12 @@ export function getAllNotes() {
 		.then((json) => {
 			const notes = json.map((note) => {
 				return `
-					<div class="note">
+					<div class="note" data-note-id="${note.id}">
 						<div class="buttons">
-							<button id='delete-button'>
-								<img src="./src/assets/close.svg" alt="delete">
+							<button class='delete-button' id="delete-button${note.id}" >
+								<img src="./src/assets/delete.svg" alt="delete">
 							</button>
-							<button id='edit-button'>
+							<button class='edit-button' id="edit-button">
 								<img src="/src/assets/pencil.svg" alt="edit">
 							</button>
 						</div>
@@ -71,13 +81,93 @@ export function getAllNotes() {
 			});
 			document.querySelector("#notes").innerHTML = notes.join("");
 			input.setAttribute("style", "background: none;");
+
+			const deleteButtons = document.querySelectorAll(".delete-button");
+			deleteButtons.forEach((button) => {
+				button.addEventListener("click", deleteNote);
+			});
+
+			const editButtons = document.querySelectorAll(".edit-button");
+			editButtons.forEach((button) => {
+				button.addEventListener("click", editNote);
+			});
 		});
 }
 
-btn.onclick = () => {
-	modalView.open();
-	modalView.btnNoElement.addEventListener("mouseover", mouseOverHandler);
-};
+function deleteNote() {
+	const noteId = this.parentNode.parentNode.getAttribute("data-note-id");
 
-let btn = document.getElementById("btn-op");
-let count = 0;
+	fetch(`http://localhost:3000/notes/${noteId}`, {
+		method: "DELETE",
+	})
+		.then((response) => {
+			if (response.ok) {
+				const noteToDelete = this.parentNode.parentNode;
+				noteToDelete.remove();
+			} else {
+				console.error("Error deleting note");
+			}
+		})
+		.catch((error) => {
+			console.error("Error deleting note", error);
+		});
+}
+
+function editNote() {
+	const noteId = this.parentNode.parentNode.getAttribute("data-note-id");
+
+	const noteTitle = this.parentNode.parentNode.querySelector("h2").textContent;
+	const noteDescription =
+		this.parentNode.parentNode.querySelector("p").textContent;
+
+	const modal = document.createElement("div");
+	modal.classList.add("modal");
+	modal.innerHTML = `
+	<div class="modal-content" id="modal-content">
+		<div class="edit-name">
+			<h3>Edit Note</h3>
+			<button id="cancel-edit-button"><img src="./src/assets/close.svg" alt="close"></button>
+		</div>
+		<form id="edit-note-form">
+			<label for="note-title">Title:</label>
+			<input type="text" id="note-title" value="${noteTitle}">
+			<label for="note-description">Description:</label>
+			<textarea id="note-description" cols="10" rows="4">${noteDescription}</textarea>
+			<button type="submit">Save Changes</button>
+		</form>
+		</div>
+	</div>
+	`;
+	document.body.appendChild(modal);
+
+	const editNoteForm = document.querySelector("#edit-note-form");
+	editNoteForm.addEventListener("submit", (event) => {
+		event.preventDefault();
+
+		const newTitle = document.querySelector("#note-title").value;
+		const newDescription = document.querySelector("#note-description").value;
+
+		fetch(`http://localhost:3000/notes/${noteId}`, {
+			method: "PUT",
+			body: JSON.stringify({
+				title: newTitle,
+				description: newDescription,
+			}),
+			headers: {
+				"Content-Type": "application/json",
+			},
+		}).then((response) => {
+			if (response.ok) {
+				const noteToEdit = this.parentNode.parentNode;
+				noteToEdit.querySelector("h2").textContent = newTitle;
+				noteToEdit.querySelector("p").textContent = newDescription;
+				modal.remove();
+			}
+		});
+	});
+	const cancelEditButton = document.querySelector("#cancel-edit-button");
+	cancelEditButton.addEventListener("click", (event) => {
+		event.preventDefault();
+		modal.remove();
+	});
+}
